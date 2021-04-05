c5320bfd9492c8dacf684d6e6c04b5d193c8cd0e
rrmckinley <mckinley1411@gmail.com>
2021-04-05 10:17:34 -0700

    Add pg dep

--- graftable/graftable-pgpool.ts
+++ graftable/graftable-pgpool.ts
@@ -1,10 +1,10 @@
-import pg from 'pg';
+import { Pool } from 'pg';
 import { GRAFTABLE_PREFIX } from './graftable-config'
 
 // LOOK: Configure DATABASE_URL here outside of graftable-config. Typically
 //       contains password not to be imported or used from client-side files.
 const { [GRAFTABLE_PREFIX + 'DATABASE_URL']: databaseUrl = 'postgres://localhost/graftable' } = process.env;
 
-const pgPool = new pg.Pool({ connectionString: databaseUrl });
+const pgPool = new Pool({ connectionString: databaseUrl });
 
 export { pgPool }
