0458f92ac6734fcdf16f18eb28a8786cb4e7dbb1
rrmckinley <mckinley1411@gmail.com>
2021-06-21 07:45:57 -0700

    Publish

--- src/graftable-pgpool.ts
+++ src/graftable-pgpool.ts
@@ -3,6 +3,7 @@ import { DEFAULT_DATABASE_URL, GRAFTABLE_PREFIX } from './graftable-config'
 
 // LOOK: Configure DATABASE_URL here outside of graftable-config. Typically
 //       contains password not to be imported or used from client-side files.
+console.log(JSON.stringify(process.env, null, 2));
 const { [GRAFTABLE_PREFIX + 'DATABASE_URL']: databaseUrl = DEFAULT_DATABASE_URL } = process.env;
 const pgPool = new Pool({ connectionString: databaseUrl });
 
