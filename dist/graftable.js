58b30a424f98b8f9c45e0fe1177fd52748fcc917
rrmckinley <mckinley1411@gmail.com>
2021-07-29 09:18:09 -0700

    Capture exit codes wip

--- dist/graftable.js
+++ dist/graftable.js
@@ -7,14 +7,13 @@ const graftable_export_schema_1 = require("./graftable-export-schema");
 const commands = {
     destroy: async () => {
         const psql = `psql postgres < ${graftable_config_server_1.databaseFile}`;
-        return await child_process_1.spawn(psql, [], {
+        const r = await child_process_1.spawn(psql, [], {
             shell: true,
             stdio: 'inherit'
         });
+        console.log(JSON.stringify(process.stderr, null, 2));
     },
-    graphql: async () => {
-        await graftable_export_schema_1.exportSchema();
-    },
+    graphql: async () => await graftable_export_schema_1.exportSchema(),
     seed: async () => {
         const seed = `echo seed`;
         await child_process_1.spawn(seed, [], {
@@ -32,15 +31,29 @@ const commands = {
 };
 const commandKeys = Object.keys(commands);
 const args = process.argv.slice(2).map(a => a.toLowerCase());
-const argErrors = args.map(a => commandKeys.includes(a) ? undefined : a);
-const hasErrors = !!argErrors.flatMap(a => a ? a : []).length;
+const argErrors = args.map(a => (commandKeys.includes(a) ? undefined : a));
+const hasErrors = !!argErrors.flatMap(a => (a ? a : [])).length;
 argErrors.map(a => {
     if (a) {
-        console.log(`Error: \`${a}\` is not a \`graftable\` command.`);
+        console.log(`Error: \`graftable ${a}\` is not a command.`);
     }
 });
 if (hasErrors) {
     process.exit(1);
 }
-args.map(a => console.log(commands[a]()));
+(async () => args.reduce(async (p, a) => {
+    const command = commands[a];
+    if (await p) {
+        return p;
+    }
+    try {
+        await command();
+        console.log(`Done: running \`graftable ${a}\` command.`);
+        return p;
+    }
+    catch (e) {
+        console.log(`Error: running \`graftable ${a}\` command.`);
+        return Promise.resolve(true);
+    }
+}, Promise.resolve(false)))();
 //# sourceMappingURL=graftable.js.map
--- src/graftable.ts
+++ src/graftable.ts
@@ -7,14 +7,13 @@ import { exportSchema } from './graftable-export-schema';
 const commands = {
   destroy: async () => {
     const psql = `psql postgres < ${databaseFile}`;
-    return await spawn(psql, [], {
+    const r = await spawn(psql, [], {
       shell: true,
       stdio: 'inherit'
     });
+    console.log(JSON.stringify(process.stderr, null, 2));
   },
-  graphql: async () => {
-    await exportSchema();
-  },
+  graphql: async () => await exportSchema(),
   seed: async () => {
     const seed = `echo seed`;
     await spawn(seed, [], {
@@ -31,7 +30,6 @@ const commands = {
   }
 };
 
-
 // TODO TS_NODE_COMPILER_OPTIONS='{\"module\":\"commonjs\"}' npx ts-node --transpile-only -r dotenv/config node_modules/graftable/dist/graftable-export-schema.js dotenv_config_path=.env.local
 
 type Command = keyof typeof commands;
@@ -39,17 +37,31 @@ type Command = keyof typeof commands;
 const commandKeys = Object.keys(commands);
 
 const args = process.argv.slice(2).map(a => a.toLowerCase());
-const argErrors = args.map(a => commandKeys.includes(a) ? undefined : a);
-const hasErrors = !!argErrors.flatMap(a => a ? a : []).length;
+const argErrors = args.map(a => (commandKeys.includes(a) ? undefined : a));
+const hasErrors = !!argErrors.flatMap(a => (a ? a : [])).length;
 
 argErrors.map(a => {
-  if(a) {
-    console.log(`Error: \`${a}\` is not a \`graftable\` command.`);
+  if (a) {
+    console.log(`Error: \`graftable ${a}\` is not a command.`);
   }
 });
 
-if(hasErrors) {
+if (hasErrors) {
   process.exit(1);
 }
 
-args.map(a => console.log(commands[a as Command]()));
+(async () =>
+  args.reduce(async (p, a) => {
+    const command = commands[a as Command];
+    if (await p) {
+      return p;
+    }
+    try {
+      await command();
+      console.log(`Done: running \`graftable ${a}\` command.`);
+      return p;
+    } catch (e) {
+      console.log(`Error: running \`graftable ${a}\` command.`);
+      return Promise.resolve(true);
+    }
+  }, Promise.resolve(false)))();
